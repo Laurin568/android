54be92a8a9d4650b0899eef06993f0cb2da3088d
liuran <liuran5@xiaomi.com>
2021-08-11 09:38:15 +0800

    first commit

---
+++ app/src/main/java/com/example/servicetest/MyIntentService.java
@@ -0,0 +1,27 @@
+package com.example.servicetest;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.JobIntentService;
+
+public class MyIntentService extends IntentService {
+
+    public MyIntentService(){
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.d("MyIntentService","Thread id is"+ Thread.currentThread().getId());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyIntentService","onDestroy executed");
+    }
+}
